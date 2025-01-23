3e0d7d37c7eb52d527227217b4c1027437429f3e
Francisco Casillas <chiccasillas@gmail.com>
2025-01-23 11:04:22 -0600

    Initial Commit

---
+++ PoemApp/PoemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemAppApp.swift
+//  PoemApp
+//
+//  Created by Francisco Jean on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PoemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
