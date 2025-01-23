e7b6b63d5e89848e0ea23505b5ff53a6712016dc
Francisco Casillas <chiccasillas@gmail.com>
2025-01-23 11:22:55 -0600

    Add color assets and spacing.

--- PoemApp/ContentView.swift
+++ PoemApp/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            TitleView()
+                .font(.custom("DancingScript-Regular", size: 40))
+                .padding()
+            PoemView()
+            Spacer()
+            Spacer()
         }
         .padding()
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        Text("My first poem")
+    }
+}
+
+struct PoemView: View {
+    var body: some View {
+        VStack {
+            Text("Roses are red,")
+                .foregroundColor(Color("Main"))
+            Text("Violets are blue,")
+                .foregroundColor(Color("Secondary"))
+            Text("So why are they called")
+                .bold()
+            Text("Violets if they're blue?")
+                .foregroundColor(Color("Third"))
+                .bold()
+                .italic()
+        }
     }
 }
 
